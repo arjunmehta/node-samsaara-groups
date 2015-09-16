c80ba8be037229b8559bbf1d6570613cd5297a27
Arjun Mehta <arjunmeht@gmail.com>
2015-09-15 19:01:54 -0700

    Updating with right content.

--- main.js
+++ main.js
@@ -1,123 +1,78 @@
 /*!
- * Samsaara Local Group Constructor
- * Copyright(c) 2014 Arjun Mehta <arjun@newlief.com>
+ * Samsaara Middleware Template
+ * Copyright(c) 2015 Arjun Mehta <arjun@arjunmehta.net>
  * MIT Licensed
  */
 
-var debug = require('debugit').add('samsaara:groups:Group');
+var debug = require('debugit').add('samsaara:groups');
 
-var samsaara;
-var executionController;
+var Group = require('./lib/group');
+var groups = {};
 
 
-function initialize(core, executionCtrl) {
-    samsaara = core;
-    executionController = executionCtrl;
+module.exports = {
 
-    return Group;
+    name: 'groups',
+
+    initialize: function(extender, capability, options) {
+
+        extender.addCoreMethods(this.coreMethods);
+        extender.addModuleMethods(this.moduleMethods);
+        extender.addConnectionInitialization(this.connectionInitialization, {
+            forced: true
+        });
+        extender.addConnectionClose(this.connectionClose);
+        extender.addMessageRoutes(this.messageRoutes);
+        extender.addPreRouteFilter(this.preRouteFilter);
+
+        createGroup('all');
+
+        return this;
+    },
+
+    coreMethods: {
+        group: group,
+        createGroup: createGroup
+    },
+
+    connectionInitialization: function(connection, done) {
+
+        debug('Initializing Groups', connection.id);
+
+        connection.groups = {};
+        groups.all.add(connection);
+
+        done();
+    },
+
+    connectionClose: function(connection) {
+
+        var connID = connection.id;
+        var groupName;
+
+        if (connection.groups) {
+
+            debug('Disconnecting Client', connID, connection.groups);
+
+            for (groupName in connection.groups) {
+                group(groupName).remove(connection);
+            }
+        }
+
+    }
+};
+
+
+function group(groupName) {
+    return groups[groupName];
 }
 
+function createGroup(groupName, members) {
+    var newGroup;
 
-function Group(groupName, members) {
+    members = members || {};
+    newGroup = new Group(groupName, members);
+    groups[groupName] = newGroup;
 
-    if (Array.isArray(members)) {
-        members = convertToObj(members);
-    }
-
-    this.members = members || {};
-    this.id = groupName;
-    this.count = Object.keys(this.members).length;
+    return newGroup;
 }
-
-
-Group.prototype.add = function(connection) {
-
-    var connectionID = connection.id;
-    var groupName = this.id;
-
-    if (this.members[connectionID] === undefined) {
-
-        this.count++;
-        this.members[connectionID] = true;
-        connection.groups[groupName] = true;
-
-        connection.nameSpace('groups').execute('addedToGroup')(this.id);
-
-        debug('Member', connectionID, 'added to:', groupName);
-
-        return true;
-    }
-
-    return false;
-};
-
-Group.prototype.remove = function(connection) {
-
-    var connectionID = connection.id;
-
-    if (this.members[connectionID] !== undefined) {
-        connection.groups[this.id] = false;
-        this.members[connectionID] = undefined;
-        this.count--;
-    }
-};
-
-// creates a namespace object that holds an execute method with the namespace as a closure..
-
-Group.prototype.nameSpace = function(namespaceName) {
-
-    var memberList = this.memberArray;
-    var _this = this;
-
-    return {
-        execute: function(funcName) {
-            executionController.execute(_this, 'GRP', memberList, namespaceName, funcName, arguments);
-        }
-    };
-};
-
-Group.prototype.execute = function(funcName) {
-    executionController.execute(this, 'GRP', this.memberArray, 'core', funcName, arguments);
-};
-
-Group.prototype.except = function(exceptionArray) {
-
-    var membersList = Object.keys(this.members);
-    var filteredMembers = membersList.filter(function(value) {
-        if (exceptionArray.indexOf(value) === -1) {
-            return value;
-        }
-    });
-
-    return new Group('filtered_' + this.id, filteredMembers);
-};
-
-Group.prototype.send = function(packet) {
-    var connectionID;
-    var members = this.members;
-
-    for (connectionID in members) {
-        if (members[connectionID] !== undefined) {
-            samsaara.connection(connectionID).socket.send(packet);
-        }
-    }
-};
-
-
-function convertToObj(array) {
-
-    var obj = {};
-    var i;
-
-    for (i = 0; i < array.length; i++) {
-        obj[array[i]] = true;
-    }
-
-    return obj;
-}
-
-
-module.exports = exports = {
-    initialize: initialize,
-    Constructor: Group
-};
